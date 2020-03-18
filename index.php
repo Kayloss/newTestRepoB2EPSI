0a18a7fba2fa8c188a479decac3f995778af9202
Kayloss <comemaryvallee@gmail.com>
2020-03-18 15:06:12 +0100

    Factorisation des fichiers/dossiers
    Création de toute l'arborescence du projet

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+<head>
+    <meta charset="UTF-8">
+    <title>Test Repo GitHub Desktop</title>
+    <link rel="stylesheet" href="assets/css/style.css"
+</head>
+<body>
+    <header>
+
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+    <script type="text/javascript" src="assets/js/script.js"></script>
+</body>
+</html>
